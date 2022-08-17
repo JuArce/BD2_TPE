f5d1959c1e9ae04ce01d5a0ce3e248c94b005c18
Julian Arce <juarce@itba.edu.ar>
2022-08-17 15:43:35 -0300

    chore: set environment variables

--- backend/crud/database.py
+++ backend/crud/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
- 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456789@localhost:5432/maverick"
+import os
+
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
  
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
